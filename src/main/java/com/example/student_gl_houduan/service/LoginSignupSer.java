6a1e74c0cbddf700a72d2d4b27a0f6ddd922f992
palpitate724 <suyang67@outlook.com>
2026-06-28 16:02:43 +0800

    2.1
    为servlte层设计logback日志系统

--- src/main/java/com/example/student_gl_houduan/service/LoginSignupSer.java
+++ src/main/java/com/example/student_gl_houduan/service/LoginSignupSer.java
@@ -5,12 +5,14 @@ import com.example.student_gl_houduan.dao.SjkMysqlDao;
 import com.example.student_gl_houduan.model.UserFanKui;
 import com.example.student_gl_houduan.model.UserStu;
 import com.example.student_gl_houduan.model.UserStuInto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Component
 public class LoginSignupSer {
@@ -23,20 +25,32 @@ public class LoginSignupSer {
         //200/yes 409/用户不存在
         userstu = sjkMysqlDao.login(userstu);
         if (userstu!=null){
-            System.out.println("login yes!"+userstu.sid+userstu.sname+userstu.spassword+userstu.sjiaose);
+            log.info("{} 登录成功",userstu.sid);
             return new UserFanKui(200,userstu);
         }
-        else return new UserFanKui(409,null);
+        else {
+            log.info("{} 登录失败，用户不存在",userstu.sid);
+            return new UserFanKui(409, null);
+        }
 
     }
 
     //注册
     public UserFanKui signupSer(UserStu userstu){
         int yz=sjkMysqlDao.signupyz(userstu);
-        if( yz==0 ) return new UserFanKui(409,null);//用户已存在
+        if( yz==0 ) {
+            log.info("{} 注册失败，用户已存在",userstu.sid);
+            return new UserFanKui(409, null);//用户已存在
+        }
         int st= sjkMysqlDao.signup(userstu);
-        if(st==1)return new UserFanKui(200,null);//注册成功
-        else return new UserFanKui(500,null);//注册失败
+        if(st==1) {
+            log.info("{} 注册成功",userstu.sid);
+            return new UserFanKui(200, null);//注册成功
+        }
+        else {
+            log.info("{} 注册失败，服务器原因",userstu.sid);
+            return new UserFanKui(500, null);//注册失败
+        }
     }
 
     //学生信息添加
@@ -44,46 +58,91 @@ public class LoginSignupSer {
         int del=sjkMysqlDao.studentIntoDel(userstuinto);
         if (del>0){
             int hf = sjkMysqlDao.studentDelHuiFu(userstuinto);
-            if (hf>0) return new UserFanKui(200,null);
-            else return new UserFanKui(500,null);
+            if (hf>0) {
+                log.info("{} 添加成功",userstuinto.sid);
+                return new UserFanKui(200, null);
+            }
+            else {
+                log.info("{} 添加失败，服务器原因",userstuinto.sid);
+                return new UserFanKui(500, null);
+            }
         }
         int cz=sjkMysqlDao.studentIntoYz(userstuinto);
-        if (cz>0) return new UserFanKui(409,null);
+        if (cz>0) {
+            log.info("{} 添加失败，信息已存在",userstuinto.sid);
+            return new UserFanKui(409, null);
+        }
         int into=sjkMysqlDao.studentInto(userstuinto);
-        if (into>0)return new UserFanKui(200,null);
-        else return new UserFanKui(500,null);
+        if (into>0) {
+            log.info("{} 添加成功",userstuinto.sid);
+            return new UserFanKui(200, null);
+        }
+        else {
+            log.info("{} 添加失败，服务器原因",userstuinto.sid);
+            return new UserFanKui(500, null);
+        }
     }
 
     //查询
     public UserFanKui studentSelectSer(UserStuInto userstuinto){
         List<UserStuInto> userstuselects=sjkMysqlDao.stuSelect(userstuinto);
-        if (userstuselects!=null){return new UserFanKui(200,userstuselects);}
-        else{return new UserFanKui(409,null);}
+        if (userstuselects!=null){
+            log.info("{} 查询成功",userstuinto.sid);
+            return new UserFanKui(200,userstuselects);
+        }
+        else{
+            log.info("{} 查询失败，信息不存在",userstuinto.sid);
+            return new UserFanKui(409,null);
+        }
     }
     //软删除查询
     public UserFanKui stuSelectDelSer(){
         List<UserStuInto> userstuselects=sjkMysqlDao.stuSelectDel();
-        if (userstuselects!=null) return new UserFanKui(200,userstuselects);
-        else return new UserFanKui(409,null);
+        if (userstuselects!=null) {
+            log.info("查询成功");
+            return new UserFanKui(200, userstuselects);
+        }
+        else {
+            log.info("查询失败，无删除信息");
+            return new UserFanKui(409, null);
+        }
     }
 
     //修改
     public UserFanKui studentUpdateSer(UserStuInto userstuinto){
         int update=sjkMysqlDao.studentUpdate(userstuinto);
-        if (update>0)return new UserFanKui(200,null);
-        else return new UserFanKui(500,null);
+        if (update>0) {
+            log.info("{} 修改成功",userstuinto.sid);
+            return new UserFanKui(200, null);
+        }
+        else {
+            log.info("{} 修改失败，服务器原因",userstuinto.sid);
+            return new UserFanKui(500, null);
+        }
     }
 
     //删除
     public UserFanKui studentDeleteSer(UserStuInto userstuinto){
         int delete=sjkMysqlDao.studentDelete(userstuinto);
-        if (delete>0)return new UserFanKui(200,null);
-        else return new UserFanKui(500,null);
+        if (delete>0) {
+            log.info("{} 删除成功",userstuinto.sid);
+            return new UserFanKui(200, null);
+        }
+        else {
+            log.info("{} 删除失败，服务器原因",userstuinto.sid);
+            return new UserFanKui(500, null);
+        }
     }
     //恢复
     public UserFanKui studentDelHuiFuSer(UserStuInto userstuinto){
         int huiFu=sjkMysqlDao.studentDelHuiFu(userstuinto);
-        if (huiFu>0)return new UserFanKui(200,null);
-        else return new UserFanKui(500,null);
+        if (huiFu>0) {
+            log.info("{} 恢复成功",userstuinto.sid);
+            return new UserFanKui(200, null);
+        }
+        else {
+            log.info("{} 恢复失败，服务器原因",userstuinto.sid);
+            return new UserFanKui(500, null);
+        }
     }
 }
